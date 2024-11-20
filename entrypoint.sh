fceeb83aee488d7244fd05d0958c68f4e2e05156
shamik789 <shamiksaha96@gmail.com>
2024-11-20 16:03:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/sh -l
 
-# Echo a hello message using the first argument passed to the script
-echo "Hello $1"
+# The argument passed to the script is a CLI command to be executed
+command="$1"
+
+# Execute the command and capture its output in the variable 'output'
+output=$(sh -c "$command")
+
+# Echo the result to the log
+echo "Command output:"
+echo "$output"
 
 # Save the output to a file
-echo $1 >> output
-
-# Print the contents of the output file to the console
-echo "Contents of output file:"
-cat output
+echo "$output" >> output
 
 # Set the output for GitHub Actions
-echo "output=$(cat output)" >> $GITHUB_ENV
-echo "::set-output name=message::$(cat output)"
+echo "output=$output" >> $GITHUB_ENV
+echo "::set-output name=message::$output"
+
