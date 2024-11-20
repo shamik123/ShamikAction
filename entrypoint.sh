bc25ed5f18ade77c5c9c1b9e02ab6da6c7ca7079
shamik789 <shamiksaha96@gmail.com>
2024-11-20 15:09:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -l
 
+# Echo a hello message using the first argument passed to the script
 echo "Hello $1"
-$1 >> output
-echo $output
-$output >> $GITHUB_OUTPUT
 
+# Save the output to a file
+echo $1 >> output
 
+# Print the contents of the output file to the console
+echo "Contents of output file:"
+cat output
 
+# Set the output for GitHub Actions
+echo "output=$(cat output)" >> $GITHUB_ENV
+echo "::set-output name=message::$(cat output)"
